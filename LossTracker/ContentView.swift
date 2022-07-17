a54c1d9e36c37617a3e8b4545ae5914a7c00f504
llieusedie <90383958+llieusedie@users.noreply.github.com>
2022-07-17 19:03:35 +0300

    Initial Commit

---
+++ LossTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LossTracker
+//
+//  Created by Paul Kirnoz on 17.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LossTracker/LossTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LossTrackerApp.swift
+//  LossTracker
+//
+//  Created by Paul Kirnoz on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LossTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
